2a537c15c98454a600705ac037e2eb0908330e72
bio <bioroobo@gmail.com>
2021-10-18 22:08:07 +0000

    in debug mode we create a route and django will give media files along this route

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,11 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from mysite import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('news/',  include('news.urls')),
 ]
+
+# in debug mode we create a route and django will give media files along this route
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
