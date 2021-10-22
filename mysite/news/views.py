34612267ce39f1cbd6f2dbdbc070ba490eb6d0e9
bio <bioroobo@gmail.com>
2021-10-22 05:29:29 +0000

    Output of all records from the News model to the site page

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse
 
+from .models import News
+
 def index(request):
-    # print(dir(request))
-    return HttpResponse('Hello index!')
+    news = News.objects.all()
+    res ='<h1>News List</h1>'
+    for item in news:
+        res += f'<div>\n<p>{item.title}</p>\n<p>{item.content}</p>\n</div>\n<hr>\n'
+        #res += f'<div><p>{item.title}</p><p>{item.content}</p></div><hr>'
+    return HttpResponse(res)
 
 def test(request):
     return HttpResponse('<h1>Test page!</h1>')
