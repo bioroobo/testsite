6b38e69ea9a6b2f95c5bf347fc8ecca7be1b9fa6
bio <bioroobo@gmail.com>
2021-10-22 06:35:12 +0000

    Create index.html template

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from news.views import *
 
 urlpatterns = [
-    path('',      index),      #   127.0.0.1:8000/news
-    path('test/', test),  #   127.0.0.1:8000/news/test
+    path('', index),  # 127.0.0.1:8000/news
 ]
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -4,12 +4,9 @@ from django.http import HttpResponse
 from .models import News
 
 def index(request):
-    news = News.objects.all()
-    res ='<h1>News List</h1>'
-    for item in news:
-        res += f'<div>\n<p>{item.title}</p>\n<p>{item.content}</p>\n</div>\n<hr>\n'
-        #res += f'<div><p>{item.title}</p><p>{item.content}</p></div><hr>'
-    return HttpResponse(res)
-
-def test(request):
-    return HttpResponse('<h1>Test page!</h1>')
+    news = News.objects.order_by('-created_at')
+    context = {
+        'news': news,
+        'title': 'News List'
+    }
+    return render(request, template_name='news/index.html', context=context)
