f46df7f7ff55af464f8eb55d1d34c37297e38b37
bio <bioroobo@gmail.com>
2021-10-26 13:59:32 +0000

    Added Category model. Added foreign key in News model

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 
-from .models import News
+from .models import News, Category
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published')
+    list_display = ('id', 'title', 'category', 'created_at', 'updated_at', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
 
 admin.site.register(News, NewsAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+
+admin.site.register(Category, CategoryAdmin)
--- mysite/news/models.py
+++ mysite/news/models.py
@@ -7,11 +7,24 @@ class News(models.Model):
     updated_at   = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
     photo        = models.ImageField(upload_to='photos/%Y/%m/%d', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
 
     def __str__(self):
         return self.title
 
     class Meta:
-        verbose_name = 'Новость(meta)'
-        verbose_name_plural = 'Новости(meta)'
+        verbose_name = 'Новость(model)'
+        verbose_name_plural = 'Новости(model)'
         ordering = ['-created_at', '-title']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование категории')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
