496aa51f736247ebf6edfb6f5d9658479f40c09f
bio <bioroobo@gmail.com>
2021-11-01 17:32:26 +0000

    Route(url) names.

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from news.views import *
 
 urlpatterns = [
-    path('', index),  # 127.0.0.1:8000/news
-    path('category/<int:category_id>/', get_category),
+    path('', index, name='home'),  # 127.0.0.1:8000/news
+    path('category/<int:category_id>/', get_category, name='category'),
 ]
