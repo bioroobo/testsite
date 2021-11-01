91bfde9d994d5490c9819fe9cc31efe9a823d235
bio <bioroobo@gmail.com>
2021-11-01 15:47:09 +0000

    Transfering categories to a template.

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import News
+from .models import News, Category
 
 def index(request):
     news = News.objects.all()
+    categories = Category.objects.all()
     context = {
         'news': news,
-        'title': 'News List'
+        'title': 'News List',
+        'category': categories,
     }
     return render(request, template_name='news/index.html', context=context)
