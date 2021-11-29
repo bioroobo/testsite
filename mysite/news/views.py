f125a8454eb406f69b738c327dcf786720f9fb6f
bio <bioroobo@gmail.com>
2021-11-29 00:20:08 +0000

    Lesson 20. Writing custom template tags: simple tags

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -5,21 +5,17 @@ from .models import News, Category
 
 def index(request):
     news = News.objects.all()
-    categories = Category.objects.all()
     context = {
         'news': news,
         'title': 'News List',
-        'categories': categories,
-    }
+     }
     return render(request, template_name='news/index.html', context=context)
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id = category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
     context = {
         'news': news,
         'category': category,
-        'categories': categories,
     }
     return render(request, template_name='news/category.html', context=context)
