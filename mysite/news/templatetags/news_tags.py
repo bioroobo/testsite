4a45a5d522ad11b3e757b5087007fc1ad1f838d2
bio <bioroobo@gmail.com>
2021-11-29 00:49:49 +0000

    Lesson 20. Writing custom template tags: inclusion tag

--- mysite/news/templatetags/news_tags.py
+++ mysite/news/templatetags/news_tags.py
@@ -7,3 +7,8 @@ register = template.Library()
 @register.simple_tag(name = 'get_list_categories')
 def get_categories():
     return Category.objects.all()
+
+@register.inclusion_tag('news/list_categories.html')
+def show_categories(arg1='Hello', arg2='world'):
+    categories = Category.objects.all() #get_categories() тоже работает#
+    return {"categories": categories, "arg1": arg1, "arg2": arg2}
